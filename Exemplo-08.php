ca750dc2658f5a04a4a5115b8fcfd4ace6e4b21b
Nery <Viniciusneryb13@hotmail.com>
2018-07-25 11:39:58 -0300

    testando github

---
+++ Exemplo-08.php
@@ -0,0 +1,12 @@
+<?php 
+
+$empresa = "Hcode";
+
+$empresa = str_replace("o", "0", $empresa);
+$empresa = str_replace("e", "3", $empresa);
+
+// substitui o valor na primeira virgula, pelo da segunda, na variavel tal.
+//var_dump(variavel) diz o tipo, quantidade de caracteres usado e printa a variavel
+echo $empresa;
+
+?>
