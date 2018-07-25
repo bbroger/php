0a415c17ce8a5ac797def58a23756e6091cdfe81
Nery <Viniciusneryb13@hotmail.com>
2018-07-25 10:40:02 -0300

    Aulas PHP

---
+++ exemplo-04.php
@@ -0,0 +1,26 @@
+<?php
+
+$a = 35;
+$b = 55;
+
+var_dump($a > $b);
+
+echo "<br>";
+
+var_dump($a < $b);
+
+echo "<br>";
+
+// var_dump($a = $b); atribuição
+
+// == igualdade valor
+
+// === igualdade identidade (tipo) 
+
+// teste verdadeiro ou falso
+
+
+
+
+
+?>
