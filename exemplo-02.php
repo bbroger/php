aac818f3e49681da54574ef6576a58deae4c5f63
Nery37 <41582582+Nery37@users.noreply.github.com>
2018-07-25 23:17:51 -0300

    Update exemplo-02.php

--- exemplo-02.php
+++ exemplo-02.php
@@ -6,7 +6,11 @@ $maluco = (int)$_GET["a"];
 
 //$ip = $_SERVER["REMOTE_ADDR"];
 
+// pega o ip do usuario
+
 $ip = $_SERVER["SCRIPT_NAME"];
+
+// pega o nome do arquivo "caminho" que o usuario acessou
 
 echo $ip;
 
