63f0cd41228aaf78899a175c9ded03bd231e6419
Nery <Viniciusneryb13@hotmail.com>
2018-08-08 13:53:24 -0300

    herança e interface

---
+++ Exemplo-68.php
@@ -0,0 +1,45 @@
+<?php
+
+interface Valida {
+
+	public function getNome();
+	
+}
+
+
+class Diretor implements Valida {
+
+	private $nome;
+
+	public function getNome(){
+
+		return $this->nome;
+	} 
+
+	public function setNome($k){
+
+		$this->nome = $k;
+	}
+
+}
+
+class Subordinado extends Diretor{
+
+	public function ponto (){
+
+		$d = $this->getNome();
+
+		echo $d;
+
+}
+}
+
+$obj = new Subordinado();
+
+$obj->setNome("Luis Eduardo");
+
+$obj->ponto();
+
+
+
+?>
