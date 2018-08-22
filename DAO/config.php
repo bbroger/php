d78e5b9d02d0fa9b7e3cfbe71a287c8c38b2fa2a
Nery <Viniciusneryb13@hotmail.com>
2018-08-22 15:21:35 -0300

    novos arquivos

--- DAO/config.php
+++ DAO/config.php
@@ -1,15 +1,13 @@
-<?php
+<?php 
 
 spl_autoload_register(function($class_name){
 
-	$filename = $class_name . ".php";
+	$filename = "class".DIRECTORY_SEPARATOR.$class_name.".php";
 
-	if (file_exists($filename)){
+	if (file_exists(($filename))) {
 		require_once($filename);
-
 	}
-
 
 });
 
-?>
+ ?>
--- DAO/index.php
+++ DAO/index.php
@@ -1,11 +1,69 @@
+<form>
+<input type = "text" name = "Login">
+<input type = "text" name = "Senha">
+<input type = "submit" value = "Confirmar">
+
+</form>
 <?php
 
 require_once("config.php");
 
-$sql = new sql();
+if (isset($_GET)){
+foreach($_GET as $key => $value) {
 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+if ($key == "Login"){
 
-echo json_encode($usuarios);
+$login = $value;
+}
 
+else {
+$password = $value;
+}
+}
+}
+
+// Criei um sisteminha de validação, "aquele projeto antigo", agr só tem que tratar a excessão, pq antes mesmo de inserir os dados ele ja ta o erro.
+
+
+//perceba que na classe usuario, ele cria um objeto da classe sql, só que la nós não temos um require da classe sql e tbm não temos um require do config.php que é o autoload e pegaria a classe automaticamente, porém, quando criamos um objeto da classe usuario aqui, o autoload faz o require dele bonitinho e tal e passa a função "autoload" pra o arquivo que ele trouxe pra ca tbm, ou seja, vc n precisa colocar outro require la, pq ele meio q ja pega esse daqui e utiliza pq a função em si vai ser retornada pra ca, então ele utiliza todos os recursos disponiveis aqui.
+
+//$kk = new Sql();
+
+//$printar = $kk->select("SELECT * FROM tb_usuarios");
+
+//echo json_encode($printar);
+
+//$teste = new Usuario("Jorgae", "JJ123");
+//$teste->Insert();
+
+$teste = new Usuario();
+
+$teste->delete(3);
+//$teste->update("jorge", "malukete", 4);
+
+/*$login = "mudança";
+$id = "3";
+$teste->update("UPDATE tb_usuarios SET deslogin = :LOGIN WHERE idusuario = :ID", array(
+':LOGIN'=>$login,
+':ID'=>$id
+));
+*/
+// devolve um usuario \/
+//$teste->loadByid(8);
+
+// com o toString la, vc pode printar o objeto direto, vai aparecer o que tiver dentro do escopo do __toString, não tente jogar o resultado pra uma variavel e printar ela, pois da erro.
+
+//echo $teste;
+// devolve uma lista de usuarios \/
+//$printando = Usuario::getList();
+
+//echo json_encode($printando);
+
+//echo json_encode(Usuario::search("A"));
+
+/*if (isset($login) && $login != '') {
+    $teste->validacao($login, $password);
+    echo $teste;
+}
+*/
 ?>
