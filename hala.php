e3d333a0de1a6f50204d8e6ecf768410d49f67f9
Nery37 <41582582+Nery37@users.noreply.github.com>
2018-09-03 23:15:12 -0300

    hala
    hihi

---
+++ hala.php
@@ -0,0 +1,46 @@
+<?php
+
+require(config.php)
+
+timer  (5 segundos)
+if (require_once(arquivo gerado no java com extensão php)){
+teste = new Usuario();
+if ($teste->LoadbyId($id)){ // << id que vem no arquivo // se retornar verdadeiro (conseguiu buscar) ele entra no if
+require_once(insereDados.php);
+}
+else(){
+echo "Usuario não cadastrado";
+}
+} 
+?>
+
+<?php
+
+arquivo insere dados \/
+$contador = 0;
+if (require_once(arquivo gerado no java com extensão php) && $contador == 0) {
+
+chama o insert e tal... printa que o alunoo entrou e etc
+
+$contador++;
+
+unlink(arquivo com extensão php);
+}
+
+if (require_once(arquivo gerado no java com extensão php) && $contador == 1 ) {
+
+faz o insert na tabela e tal... printa que o aluno saio 
+
+$contador--;
+
+unlink(arquivo com extensão php);
+
+} 
+
+
+?>
+
+obs: o arquivo gerado pelo java (semelhante a função fopen no php) tem que ser um arquivo php e conter um json com o codigo 
+da tag... (vc tem que vincular a tag com um usuario)
+
+obs: ver dps como o linkedin faz pra aparecer notificações no meu celular só por eu estar no logado no site por ele. 
