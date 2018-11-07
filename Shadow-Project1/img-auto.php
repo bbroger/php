86f8d850e50a44d8638506a61c7b1d97cfbd5a0c
Vinicius Nery <Viniciusneryb13@hotmail.com>
2018-11-07 14:23:35 -0300

    Shadow Project

--- Shadow-Project1/img-auto.php
+++ Shadow-Project1/img-auto.php
@@ -10,11 +10,11 @@ $gray = imagecolorallocate($image, 100, 100, 100);
 
 imagettftext($image, 30, 0, 55, 195, $titleColor, __DIR__ .DIRECTORY_SEPARATOR.'fonts'.DIRECTORY_SEPARATOR.'Black'.DIRECTORY_SEPARATOR.'Black-Jack1.ttf', utf8_decode("Conta Cadastrada :" ));
 
-imagettftext($image, 20, 0, 55, 231, $titleColor, __DIR__ .DIRECTORY_SEPARATOR.'fonts'.DIRECTORY_SEPARATOR.'Playball'.DIRECTORY_SEPARATOR.'Playball-Regular.ttf', utf8_decode("-> " . $conta));
+imagettftext($image, 23, 0, 55, 231, $titleColor, __DIR__ .DIRECTORY_SEPARATOR.'fonts'.DIRECTORY_SEPARATOR.'Playball'.DIRECTORY_SEPARATOR.'Playball-Regular.ttf', utf8_decode("-> " . $conta));
 
 imagettftext($image, 30, 0, 55, 282, $titleColor, __DIR__ .DIRECTORY_SEPARATOR.'fonts'.DIRECTORY_SEPARATOR.'Black'.DIRECTORY_SEPARATOR.'Black-Jack1.ttf', utf8_decode("Senha Cadastrada :" ));
 
-imagettftext($image, 20, 0, 55, 327, $titleColor, __DIR__ .DIRECTORY_SEPARATOR.'fonts'.DIRECTORY_SEPARATOR.'Playball'.DIRECTORY_SEPARATOR.'Playball-Regular.ttf', utf8_decode("-> " . $senha));
+imagettftext($image, 23, 0, 55, 327, $titleColor, __DIR__ .DIRECTORY_SEPARATOR.'fonts'.DIRECTORY_SEPARATOR.'Playball'.DIRECTORY_SEPARATOR.'Playball-Regular.ttf', utf8_decode("-> " . $senha));
 
 imagettftext($image, 62, 0, 80, 470, $titleColor, __DIR__ .DIRECTORY_SEPARATOR.'fonts'.DIRECTORY_SEPARATOR.'evafangoriasuperital'.DIRECTORY_SEPARATOR.'evafangoriasuperital.ttf', utf8_decode("SHADOW" ));
 
