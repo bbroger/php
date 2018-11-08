947a849caa5487c37492068e792cf776d5cd7a7c
Vinicius Nery <Viniciusneryb13@hotmail.com>
2018-11-08 12:36:33 -0300

    Shadow Project 2

--- Curl/index.php
+++ Curl/index.php
@@ -1,5 +1,7 @@
 <form>
 	<input type="number" name="CEP">
+		<input type="number" name="Tuta">
+			<input type="number" name="Tete">
 	<input type="submit" value="GO">
 
 </form>
@@ -7,8 +9,8 @@
 <?php
 
 require_once("config.php");
-
-$Cep;
+print_r($_GET['Tuta']);
+/*$Cep;
 if (isset($_GET)){
 foreach ($_GET as $key => $value) {
 	$Cep = $value;
@@ -18,7 +20,9 @@ if(isset($Cep) && $Cep!=''){
 $teste = new Valid();
 //print_r($teste->info($Cep));
 echo $teste->info($Cep);
+
 }
+*/
 
 
 ?>
